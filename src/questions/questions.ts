1fc494cfdccc8683628b50cfcadbd6f8e11e4d44
Mekerjunior <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>
2023-10-23 01:51:55 -0300

    update

--- src/questions/questions.ts
+++ src/questions/questions.ts
@@ -1,36 +1,34 @@
 import { EChoicesBoilerPlate } from 'enuns/choices.boilers.enum';
 import path from 'node:path';
-import fs from 'node:fs'
+import fs from 'node:fs';
+
 export const questions = [
   {
     type: 'list',
     name: 'tech',
-    message: 'Qual Boilerplate deseja?',
+    message: 'Which Boilerplate do you want to use?',
     choices: [EChoicesBoilerPlate.NODE_JS, EChoicesBoilerPlate.Scss],
   },
   {
     type: 'input',
     name: 'techName',
-    message: 'Dê um nome para a pasta do projeto:',
+    message: 'Give a name to the project folder:',
     validate: (folderName: string) => {
       if (/[^\w\s-]/.test(folderName)) {
-        return 'Nome inválido! Use caracteres validos';
-      }else if(!folderName){
-        return "Por favor insira o nome";
-      }else if (folderName === "boilerplate-typescript-nodejs" || 
-                folderName == "boilerplate-scss") {
-                  return "Não é possivel criar com esse nome.";
+        return 'Invalid name! Use valid characters.';
+      } else if (!folderName) {
+        return 'Please enter a name.';
+      } else if (folderName === 'boilerplate-typescript-nodejs' || folderName === 'boilerplate-scss') {
+        return 'Cannot create with this name.';
+      } else {
+        try {
+          const dir = path.resolve(folderName);
+          fs.accessSync(dir, fs.constants.R_OK);
+          return 'Folder with the same name already exists.';
+        } catch (error) {
+          // Folder doesn't exist, so it's a valid name.
+        }
       }
-
-      try {
-        const dir = path.resolve(folderName);
-        fs.accessSync(dir, fs.constants.R_OK);
-        console.log(dir)
-      } catch (error) {
-        return "Nome invalido";
-      }
-
-    
 
       // You can add more custom validation logic here if needed.
       return true; // Return true to indicate the input is valid.
